fd68c48f77bd3232684ae9f1fe2bc7ea6fb73a5d
imlaufin <imlaufin@gmail.com>
2025-08-14 16:11:42 +0530

    Update ui_suppliers.py

--- fabric_tracker_tk/ui_suppliers.py
+++ fabric_tracker_tk/ui_suppliers.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-import db
+from fabric_tracker_tk import db
+
 
 class SuppliersScreen(ttk.Frame):
     def __init__(self, parent, controller):
